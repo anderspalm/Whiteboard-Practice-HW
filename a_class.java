738998031e7e9b8357ee1d0fa5cf1b606a8d8f8f
Anders Palm <ang.palm@hotmail.com>
2016-07-06 21:56:28 -0400

    added homework to directory

---
+++ a_class.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+/**
+ * Created by andeski on 7/5/16.
+ */
+public class a_class {
+
+    public static void main(String[] args) {
+        doubleChar("Rabbit");
+    }
+
+    public static String doubleChar(String str) {
+
+     char name[] =  str.toCharArray();
+
+
+
+    }
+
+
+}
